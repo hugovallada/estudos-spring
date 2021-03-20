fd269ae7abd8a80a964572676cc90c9020ee13e9
hugovallada <valladahugo@gmail.com>
2021-03-20 11:37:16 -0300

    Adiciona status de resposta aos métodos

--- tratamento-excessoes/src/main/java/com/github/hugovallada/tratamentoexcessoes/exception/HandleExceptions.java
+++ tratamento-excessoes/src/main/java/com/github/hugovallada/tratamentoexcessoes/exception/HandleExceptions.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.github.hugovallada.tratamentoexcessoes.exception.custom.EntityAlreadyExistException;
 
@@ -17,6 +18,7 @@ import com.github.hugovallada.tratamentoexcessoes.exception.custom.EntityAlready
 public class HandleExceptions {
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		List<String> errors = new ArrayList<>();
 		
@@ -34,16 +36,19 @@ public class HandleExceptions {
 	}
 	
 	@ExceptionHandler(NoSuchElementException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
 	public ApiError handleNoSuchElementException(NoSuchElementException exception) {
-		return buildApiError(HttpStatus.BAD_REQUEST, exception.getMessage(), Collections.singletonList(exception.getMessage()));
+		return buildApiError(HttpStatus.NOT_FOUND, exception.getMessage(), Collections.singletonList(exception.getMessage()));
 	}
 	
 	@ExceptionHandler(DataIntegrityViolationException.class)
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 	public ApiError handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
 		return buildApiError(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), Collections.singletonList(exception.getMessage()));
 	}
 	
 	@ExceptionHandler(EntityAlreadyExistException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	public ApiError handleEntityAlreadyExistsException(EntityAlreadyExistException exception) {
 		return buildApiError(HttpStatus.BAD_REQUEST, exception.getMessage(), Collections.singletonList(exception.getMessage()));
 	}
