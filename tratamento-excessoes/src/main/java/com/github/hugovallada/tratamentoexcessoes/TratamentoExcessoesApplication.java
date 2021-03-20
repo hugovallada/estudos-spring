6155cd7fe436113878528f10a7b36d5bebd3fb73
hugovallada <valladahugo@gmail.com>
2021-03-20 08:58:34 -0300

    Cria o repositório de estudos

---
+++ tratamento-excessoes/src/main/java/com/github/hugovallada/tratamentoexcessoes/TratamentoExcessoesApplication.java
@@ -0,0 +1,13 @@
+package com.github.hugovallada.tratamentoexcessoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TratamentoExcessoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TratamentoExcessoesApplication.class, args);
+	}
+
+}
---
+++ tratamento-excessoes/src/test/java/com/github/hugovallada/tratamentoexcessoes/TratamentoExcessoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hugovallada.tratamentoexcessoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TratamentoExcessoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
