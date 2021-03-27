28bffadbf14216539ef53facbb37e43a7aa7e6db
hugovallada <valladahugo@gmail.com>
2021-03-27 11:23:11 -0300

    Cria a documentação do Swagger para o ResponseDTO

--- documentacao/src/main/java/com/github/hugovallada/documentacao/dto/CarroResponseDTO.java
+++ documentacao/src/main/java/com/github/hugovallada/documentacao/dto/CarroResponseDTO.java
@@ -2,25 +2,34 @@ package com.github.hugovallada.documentacao.dto;
 
 import com.github.hugovallada.documentacao.entity.Carro;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel("Dados Retornados sobre o Carro")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CarroResponseDTO {
 	
+	@ApiModelProperty(value = "Código de identificação do carro")
 	private Long id;
 	
+	@ApiModelProperty(value = "Marca do carro")
 	private String marca;
 	
+	@ApiModelProperty(value = "Ano de fabricação do carro")
 	private Integer ano;
 	
+	@ApiModelProperty(value = "Número de passageiros que podem ser transportados no carro")
 	private Integer numeroPassageiros;
 	
+	@ApiModelProperty(value = "Quantos quilômetros o carro já rodou")
 	private Double kmRodados;
 	
+	@ApiModelProperty(value = "Quantos donos o carro já teve")
 	private Integer numDonos;
 	
 	
