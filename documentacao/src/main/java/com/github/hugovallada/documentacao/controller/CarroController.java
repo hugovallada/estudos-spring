efa0d754d32adc281851b89df0eb5494c6d27f98
hugovallada <valladahugo@gmail.com>
2021-03-27 11:18:43 -0300

    Criar os métodos GET e POST e adiciona tags ao swagger do controller

--- documentacao/src/main/java/com/github/hugovallada/documentacao/controller/CarroController.java
+++ documentacao/src/main/java/com/github/hugovallada/documentacao/controller/CarroController.java
@@ -1,11 +1,26 @@
 package com.github.hugovallada.documentacao.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.hugovallada.documentacao.dto.CarroRequestDTO;
+import com.github.hugovallada.documentacao.dto.CarroResponseDTO;
 import com.github.hugovallada.documentacao.service.CarroService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Carro")
 @RestController
 @RequestMapping("/carro")
 public class CarroController {
@@ -15,5 +30,18 @@ public class CarroController {
 	@Autowired
 	public CarroController(CarroService carroService) {
 		this.carroService = carroService;
+	}
+	
+	@ApiOperation(value = "Listar Carros", nickname = "listarTodosCarros")
+	@GetMapping
+	public List<CarroResponseDTO> buscarTodos(){
+		return carroService.buscarTodos();
+	}
+	
+	@ApiOperation(value = "Criar novo Carro", nickname = "criarNovoCarro")
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public CarroResponseDTO criar(@RequestBody @Valid CarroRequestDTO carroRequest) {
+		return carroService.criar(carroRequest);
 	}
 }
--- documentacao/src/main/java/com/github/hugovallada/documentacao/service/CarroService.java
+++ documentacao/src/main/java/com/github/hugovallada/documentacao/service/CarroService.java
@@ -1,8 +1,15 @@
 package com.github.hugovallada.documentacao.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.hugovallada.documentacao.dto.CarroRequestDTO;
+import com.github.hugovallada.documentacao.dto.CarroResponseDTO;
+import com.github.hugovallada.documentacao.entity.Carro;
 import com.github.hugovallada.documentacao.repository.CarroRepository;
 
 @Service
@@ -13,6 +20,18 @@ public class CarroService {
 	@Autowired
 	public CarroService (CarroRepository carroRepository) {
 		this.carroRepository = carroRepository;
+	}
+	
+	public List<CarroResponseDTO> buscarTodos(){
+		List<Carro> carros = carroRepository.findAll();
+		return carros.stream()
+				.map(carro -> CarroResponseDTO.toDto(carro))
+				.collect(Collectors.toList());
+	}
+	
+	public CarroResponseDTO criar(CarroRequestDTO carroRequestDTO) {
+		Carro carro = carroRepository.save(carroRequestDTO.toModel());
+		return CarroResponseDTO.toDto(carro);
 	}
 	
 	
